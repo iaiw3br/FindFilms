703b2fcec61758a774de7c5b822c7be2b9911f30
ArtyomIvanov <iaiw3br@gmail.com>
2020-02-03 20:56:07 +0300

    add prop-types

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
 
 const Header = ({ text }) => (
   <header className="App-header">
     <h2>{text}</h2>
   </header>
 );
+
+Header.propTypes = {
+  text: PropsTypes.string.isRequired,
+};
 
 export default Header;
